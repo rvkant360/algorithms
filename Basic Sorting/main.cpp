26a689320977f0b42a3c2096dbbb04a1053bd5a2
Ravi Kant <rvkant360@gmail.com>
2025-04-24 11:35:41 +0530

    Create  main.cpp
    The program provides 3 basic sorting
    - Bubble sort
    - Selection sort
    - Insertion sort

---
+++ Basic Sorting/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+// O(n^2)
+void bubbleSort(int arr[], int size) {
+    for (int i = 0; i < size -1; i++) {
+        for (int j = 0; j < size -1 -i; j++) {
+            if (arr[j] > arr[j+1]) {
+                std::swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+}
+
+// O(n^2)
+void selectionSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        int smallest = i;
+        for (int j = i+1; j < size; j++) {
+            if (arr[j] < arr[smallest]) {
+                smallest = j;
+            }
+        }
+        std::swap(arr[i], arr[smallest]);
+    }
+}
+
+// O(n^2)
+void insertionSort(int arr[], int size) {
+     for (int i = 1; i < size; i++) {
+        int curr = arr[i];
+        int prev = i - 1;
+        while (prev >= 0 && arr[prev] > curr) {
+            arr[prev + 1] = arr[prev];
+            prev--;
+        }
+        arr[prev + 1] = curr;
+     }
+}
+
+int main() {
+
+    int arr[5] = {1,4,5,2,3};
+    insertionSort(arr, 5);
+
+    for(int a : arr) {
+        std::cout << a << std::endl;
+    }
+
+    return 0;
+}
